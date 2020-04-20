6bb3f36ab2375e126d4920962872cbc7c384d15e
Evan Roman <evanroman1@gmail.com>
2020-04-20 20:08:20 +0000

    use ERT tests

--- set-1/challenge-1.el
+++ set-1/challenge-1.el
@@ -39,5 +39,3 @@
         (setq result (concat result four-b64))))
     result))
 
-(print (hex-to-b64 "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"))
-
---
+++ test-set-1.el
@@ -0,0 +1,13 @@
+;; run with emacs -batch -l ert -l test-set-1.el -f ert-run-tests-batch-and-exit
+(defun load-directory (dir)
+  (let ((load-it (lambda (f)
+                   (load-file (concat (file-name-as-directory dir) f)))
+                 ))
+    (mapc load-it (directory-files dir nil "\\.el$"))))
+(load-directory "./set-1")
+(ert-deftest challenge-1 ()
+  (should (string= (hex-to-b64
+              "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+              )
+             "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+             )))
