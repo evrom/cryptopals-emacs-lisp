693bb1f78e6ccacfac4340347444723b5a40c6aa
Evan Roman <evanroman1@gmail.com>
2020-04-28 00:13:49 +0000

    implement hamming distance

---
+++ set-1/challenge-6.el
@@ -0,0 +1,48 @@
+(defun int-to-bin-str (i)
+  "convert an integer into it's binary representation in string format"
+  (let ((result ""))
+    (while (not (= i 0))
+      (setq result (concat (if (= 1 (logand i 1)) "1" "0") result))
+      (setq i (lsh i -1)))
+    (if (string= result "")
+        (setq result "0"))
+    result))
+
+(defun pad-bin-str (str)
+  (concat (make-string (- 8 (length str)) ?0) str)
+  )
+
+(defun hamming-distance-byte (byte-1 byte-2)
+  (let
+      (
+       (result 0)
+       (bin-str-1 (pad-bin-str (int-to-bin-str byte-1)))
+       (bin-str-2 (pad-bin-str (int-to-bin-str byte-2)))
+       )
+    (dotimes (i 8)
+      (if (= (aref bin-str-1 i) (aref bin-str-2 i))
+          nil
+        (setq result (+ result 1))
+        )
+      )
+    result
+    ))
+
+(defun hamming-distance (str-1 str-2)
+  (let
+      (
+       (result 0)
+       )
+    (dotimes (i (length str-1))
+      (setq result
+            (+ result
+               (hamming-distance-byte
+                (aref str-1 i)
+                (aref str-2 i)
+                )
+               )
+            )
+      )
+    result
+    )
+  )
--- test-set-1.el
+++ test-set-1.el
@@ -54,3 +54,9 @@ I go crazy when I hear a cymbal"
            "ICE")
            "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"))
   )
+
+;; challenge 5 helper functions
+
+(ert-deftest hamming-distance ()
+  (should (= (hamming-distance "this is a test" "wokka wokka!!!") 37))
+  )
